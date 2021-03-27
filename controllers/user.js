e18a4b408e664ef574b98aaf647efa0d9ef24291
Nicolás Soncinim <nicosoncini15@hotmail.com>
2021-03-27 16:22:32 -0300

    a ver

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,43 @@
 const User = require('../models/user')
 
 const getUser = async (req, res) => {
-    const users = await User.find()
-    res.send(users)
+    try{
+        const users = await User.find();
+        res.send(users)}
+        catch(error) {
+            console.log(error);
+        }
 }
 
 const createUser = async (req, res) => {
-        let user = await new User();
-        user.nombre = req.body.nombre;
-        user.apellido = req.body.apellido;
-        user.save();
-        res.send('usuario creado');
+        try{
+            let user = await new User();
+            user.nombre = req.body.nombre;
+            user.apellido = req.body.apellido;
+            user.save();
+            res.send('usuario creado');}
+            catch(error) {
+                console.log(error)
+            }
 }
 
 const updateUser = async (req, res) => {
-    const {nombre, apellido}= req.body;
-    await User.findByIdAndUpdate(req.params.id, {nombre, apellido});
-    res.send('usuario editado');
+    try{    
+        const {nombre, apellido}= req.body;
+        await User.findByIdAndUpdate(req.params.id, {nombre, apellido});
+        res.send('usuario editado');}
+        catch(error) {
+            console.log(error)
+        }
 }
 
 const deleteUser = async (req, res) => {
-    const {nombre, apellido}= req.body;
-    await User.findByIdAndDelete(req.params.id, {nombre, apellido});
-    res.send('usuario borrado');
+    try{
+        const {nombre, apellido}= req.body;
+        await User.findByIdAndDelete(req.params.id, {nombre, apellido});
+        res.send('usuario borrado');}
+        catch(error) {
+            console.log(error)}
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //Server
 const server = require('./server')
-server.listen(process.env.PORT, () => {
+server.listen(3000, () => {
     console.log(`Server escuchando en puerto ${process.env.PORT}`);
 });
+
+//process.env.PORT
