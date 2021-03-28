86217bf2cc385b6afaddb53bc62184263851e30c
Nicolás Soncinim <nicosoncini15@hotmail.com>
2021-03-28 20:51:27 -0300

    a ver

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 //Inicialización
 const express = require ('express');
 const server = express();
-const dbConnect = require('./database')
+//const dbConnect = require('./database')
 
 server.use(express.json());
 
 //Rutas
-const userRoutes = require('./routes/user')
+/*const userRoutes = require('./routes/user')
 const clientRoutes = require ('./routes/client')
 const turnoRoutes = require('./routes/turno')
 
 server.use('/user', userRoutes)
 server.use('/client', clientRoutes)
 server.use('/turno', turnoRoutes)
-
+*/
 module.exports = server
